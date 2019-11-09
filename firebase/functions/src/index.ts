d9ea8a0332080fa4fa9aa6c5fe0e87882434b830
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-11-09 16:50:46 +0900

    apiのエンドポイントだけオラッと作成

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -3,6 +3,18 @@ import { region, Request, Response } from 'firebase-functions';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const sample = region('asia-northeast1').https.onRequest((request: Request, response: Response) => {
+export const getItem = region('asia-northeast1').https.onRequest((request: Request, response: Response) => {
   response.json({ state: 'success' });
+});
+
+export const listItems = region('asia-northeast1').https.onRequest((request: Request, response: Response) => {
+  response.json([{ state: 'success' }]);
+});
+
+export const updateItem = region('asia-northeast1').https.onRequest((request: Request, response: Response) => {
+  if (request.method !== 'POST') {
+    response.send('This is not post request')
+  }
+
+  response.send('This is post request')
 });
