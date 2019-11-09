95e04e1940f8d8d657685a19728098f88b3cdfc7
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-11-09 12:16:44 +0900

    add formatter and execute

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,8 +1,8 @@
-import {region, Request, Response} from "firebase-functions";
+import { region, Request, Response } from 'firebase-functions';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const sample = region("asia-northeast1").https.onRequest((request: Request, response: Response) => {
-  response.json({state: "success"});
+export const sample = region('asia-northeast1').https.onRequest((request: Request, response: Response) => {
+  response.json({ state: 'success' });
 });
