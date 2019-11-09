58ac301671e3cf7b98f59cc22c8ae86fbf32bfdf
abyoy <abyoyororo@gmail.com>
2019-11-09 20:33:25 +0900

    [add]

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    private GameObject _parent;
+    private ItemMaster manager;
+    private MeshRenderer renderer;
+
+    void Start()
+    {
+
+        //親オブジェクトを取得
+        _parent = transform.root.gameObject;
+        manager = _parent.GetComponent<ItemMaster>();
+        renderer = this.GetComponent<MeshRenderer>();
+
+    }
+
+    void Update()
+    {
+        Debug.Log(manager.isItemShow);
+        if (!manager.isItemShow)
+        {
+            renderer.enabled = (false);
+        }
+        else
+        {
+            renderer.enabled = (true);
+        }
+    }
+
+   void log()
+    {
+        Debug.Log("aa");
+    }
+    
+
+}
+
---
+++ Assets/Scripts/ItemMaster.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemMaster : MonoBehaviour
+{
+
+    public bool isItemShow;
+    // Start is called beore the first frame update
+    void Start()
+
+    {
+        isItemShow = true;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        this.isItemShow = true;
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        this.isItemShow = false;
+    }
+}
