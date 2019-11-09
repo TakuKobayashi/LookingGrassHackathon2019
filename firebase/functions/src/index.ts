92e9e6da7a800b02381e8dfc45d708ff5a9107c4
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-11-09 12:04:36 +0900

    add sample function

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -3,6 +3,6 @@ import * as functions from 'firebase-functions';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const sample = functions.https.onRequest((request: functions.Request, response: functions.Response) => {
+  response.json({state: "success"});
+});
