21ea62c96e8fac67c4d7f16e9d563b45ad610a9c
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-11-09 18:34:58 +0900

    一応、Character制御用の処理を追加

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] private GameObject characterModelObj;
+
+    private GameObject threedObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.threedObject = ComponentUtil.InstantiateTo(this.gameObject, characterModelObj);
+    }
+
+    public void InputPreviewTarget(Vector3 targetPosition)
+    {
+        StartCoroutine(LookAtPreview(targetPosition));
+    }
+
+    private IEnumerator LookAtPreview(Vector3 targetPos)
+    {
+        while(this.threedObject == null)
+        {
+            yield return null;
+        }
+//        this.threedObject.transform.position = targetPos;
+        this.threedObject.transform.position = targetPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using LookingGlass;
+
+public class CharacterController : SingletonBehaviour<CharacterController>
+{
+    [SerializeField] private GameObject characterObj;
+    [SerializeField] private Holoplay previewField;
+
+    public Holoplay PreviewField
+    {
+        get
+        {
+            return previewField;
+        }
+    }
+
+    private List<Character> characters = new List<Character>();
+
+    void Start()
+    {
+        Character character = ComponentUtil.InstantiateTo<Character>(this.gameObject, characterObj);
+        Vector3 targetPosition = previewField.transform.position;
+        character.InputPreviewTarget(targetPosition);
+        characters.Add(character);
+    }
+}
